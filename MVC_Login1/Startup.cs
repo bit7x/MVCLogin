7588bc18b4f7fd91fceb60de84b5ca54965304b7
hirucool <viranja@live.com>
2014-11-19 12:03:11 +0530

    <init>

---
+++ MVC_Login1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Login1.Startup))]
+namespace MVC_Login1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
